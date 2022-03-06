938e1ff6d7f07b185294f306e242699958531210
Alex <sepp.amsl@protonmail.com>
2022-03-06 23:54:25 +0100

    Fixed redeploy script and nav <li>

--- redeploy.ps1
+++ redeploy.ps1
@@ -1,6 +1,6 @@
 pushd .\GippDev\Server
-dotnet publish -c Release -r linux-x64
+dotnet publish -r linux-x64
 pushd .\bin\Release\net6.0\linux-x64\publish
 scp -r * root@gipp.dev:/var/www/gipp
-systemctl restart gippweb
+ssh root@gipp.dev "systemctl restart gippweb"
 popd
