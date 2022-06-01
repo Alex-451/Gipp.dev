7eaf21acc3fa29c7e038c2ca4a58362f8e2ea81f
Alex <sepp.amsl@protonmail.com>
2022-06-01 11:41:05 +0200

    Redeploy script

--- redeploy.ps1
+++ redeploy.ps1
@@ -1,5 +1,5 @@
 pushd .\GippDev\Server
-dotnet publish -r linux-x64
+dotnet publish -c Release -r linux-x64
 pushd .\bin\Release\net6.0\linux-x64\publish
 scp -r * root@gipp.dev:/var/www/gipp
 ssh root@gipp.dev "systemctl restart gippweb"
