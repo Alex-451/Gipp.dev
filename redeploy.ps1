0246bb813b80bc477893b13736fe2e06b2e417af
Alex <sepp.amsl@protonmail.com>
2023-01-13 14:12:01 +0100

    Fixed spelling mistake

--- redeploy.ps1
+++ redeploy.ps1
@@ -1,6 +1,6 @@
 pushd .\GippDev\Server
 dotnet publish -c Release -r linux-x64
 pushd .\bin\Release\net6.0\linux-x64\publish
-scp -r * root@gipp.dev:/var/www/gipp
+scp -r * root@gipp.dev:/var/www/gipp-dev
 ssh root@gipp.dev "systemctl restart gippweb"
 popd
